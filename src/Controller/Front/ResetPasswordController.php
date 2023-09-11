1a05f617edbff7f9bee129d7ae45994cb115fc27
laurasullyalexandrine <launyie@gmail.com>
2023-09-11 21:20:19 +0200

    add ResetPasswordController, reset template and updated the login template

---
+++ src/Controller/Front/ResetPasswordController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ResetPasswordController extends AbstractController
+{
+    #[Route('/reset/password', name: 'reset_password')]
+    public function reset(Request $request): Response
+    {
+        $form = $this->createForm(ResetPasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Créer un service pour :
+                // Vérifier qu'il existe des utilisateurs en bdd
+                // Vérifier le nom de l'utilisateur existe en bdd
+
+                // Si il existe envoyer l'email
+                    // Ajouter le service pour envoyer l'email de réinitialisation.
+        }
+
+        return $this->render('front/reset_password/reset.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
