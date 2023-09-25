33e825fa837dd26aadaabe264a0b618c43d31565
laurasullyalexandrine <launyie@gmail.com>
2023-09-25 21:37:59 +0200

    implementation of a mediatrait to centralize identical properties and methods between the Image and Video entity

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\MediaTrait;
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -10,104 +11,5 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 #[ORM\HasLifecycleCallbacks]
 class Image
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
-
-    #[ORM\Column]
-    private ?\DateTimeImmutable $created_at = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?\DateTimeImmutable $updated_at = null;
-
-    #[ORM\ManyToOne(inversedBy: 'images', cascade: ['persist'])]
-    private ?Trick $trick = null;
-
-    private ?UploadedFile $uploadedFile = null;
-
-    public function __construct()
-    {
-        $this->created_at = new \DateTimeImmutable();
-    }
-
-    public function __toString()
-    {
-        return $this->name;
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->created_at;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $created_at): static
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updated_at;
-    }
-
-    public function setUpdatedAt(?\DateTimeImmutable $updated_at): static
-    {
-        $this->updated_at = $updated_at;
-
-        return $this;
-    }
-
-    public function getTrick(): ?Trick
-    {
-        return $this->trick;
-    }
-
-    public function setTrick(?Trick $trick): static
-    {
-        $this->trick = $trick;
-
-        return $this;
-    }
-
-    /**
-     * Function to edit name of image upload
-     */
-    #[ORM\PrePersist]
-    public function setFileName(): void
-    {
-        $this->uploadedFile = new UploadedFile($this->name, 'test', null, null, true); 
-        $this->name = uniqid() . '.' . $this->uploadedFile->guessExtension();
-    }
-
-    /**
-     * Function to save the recorded file
-     */
-    #[ORM\PostPersist]
-    public function saveFile(): void
-    {
-        $this->uploadedFile->move(__DIR__ . '/../../public/upload/image', $this->name);
-    }
+    use MediaTrait;
 }
